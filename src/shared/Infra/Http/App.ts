0f04eef8cc7f38402d5e3d3ba51518a2b37313f4
douglasor <douglasor@outlook.com>
2022-07-20 09:35:56 -0300

    Add rate limiter

--- src/shared/Infra/Http/App.ts
+++ src/shared/Infra/Http/App.ts
@@ -12,16 +12,17 @@ import { AppError } from "@shared/errors/AppError";
 import swaggerFile from "../../../docs/swagger.json";
 import { routes } from "./Routes";
 import upload from "@config/upload";
+import rateLimiter from "@shared/Infra/Http/middleware/rateLimiter";
 
 
 
 
 const app = express();
-
+app.use(rateLimiter);
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use("/avatar",express.static(`${upload.tmpFolder}/avatar`));
-app.use("/cars",express.static(`${upload.tmpFolder}/cars`))
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
 app.use(routes)
 
@@ -31,7 +32,7 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     if (err instanceof AppError) {
         return response.status(err.statusCode).json({ message: err.message });
     }
-    console.log({err})
+    console.log({ err })
     return response.status(500).json({
         status: "Error",
         message: `Internal Server Errors ${err.message}`
